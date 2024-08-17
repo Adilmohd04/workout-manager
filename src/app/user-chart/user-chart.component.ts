8b36119e64c9f2498644ff7b73ebd59705a31726
Adilmohd04 <132822984+Adilmohd04@users.noreply.github.com>
2024-08-17 10:10:27 +0530

    Update user-chart.component.ts

--- src/app/user-chart/user-chart.component.ts
+++ src/app/user-chart/user-chart.component.ts
@@ -35,7 +35,7 @@ export class UserChartComponent implements OnChanges, OnInit {
     if (this.isBrowser) {
       if (changes['selectedUser'] || changes['users']) {
         if (this.users.length === 0) {
-          this.selectedUser = null; // Ensure selectedUser is null if no user
+          this.selectedUser = null; 
           this.clearChart();
         } else if (!this.selectedUser || !this.users.some(user => user.id === this.selectedUser?.id)) {
           this.selectedUser = this.getDefaultUser();
